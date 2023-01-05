70fc72bf9ae8291e719d63b1322cd81633225b03
Đỗ Văn Minh Dũng <81916947+dovanminhdung2001@users.noreply.github.com>
2023-01-05 10:30:33 +0700

    demo excel

--- Demo export excell/src/main/java/excel1/BookEntity.java
+++ Demo export excell/src/main/java/excel1/BookEntity.java
@@ -1,4 +1,12 @@
 package excel1;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class BookEntity {
+    private Long id;
+    private String name;
+    private Double price;
 }
