cb0400f28180f14a1c9f788608181958efd90b54
dovanminhdung2001@gmail.com <140503031711As>
2023-10-04 16:49:51 +0700

    cf  dev

--- demo git conflict/src/main/java/Cal.java
+++ demo git conflict/src/main/java/Cal.java
@@ -3,14 +3,14 @@ import java.util.List;
 
 public class Cal {
     static void findNum(List<Integer> Arr) {
-        int num = Arr.get(0);
+        int arr = Arr.get(0);
         for (int i = 0; i < Arr.size(); i++) {
             int val = Arr.get(i);
-            if (num < val) {
-                num = val;
+            if (arr < val) {
+                arr = val;
             }
         }
-        System.out.println("max = " + num);
+        System.out.println("max = " + arr);
     }
     public static void main(String[] args) {
         List<Integer> Arr = new ArrayList<>(List.of(new Integer[]{1, 2, 1, 0, -1, 4}));
